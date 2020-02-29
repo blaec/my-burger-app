16ab3506403e7de014f8e23b195f2766f2a25253
blaec <odeskonst@yandex.ru>
2020-02-29 19:50:18 +0200

    276. Showing Error Messages

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -39,9 +39,15 @@ const input = (props) => {
                                   {...props.elementConfig}
                                   value={props.value}/>;
     }
+    let validationError = null;
+    if (props.invalid && props.touched) {
+        validationError = <p className={classes.ValidationError}>Please enter a valid {props.elementType}</p>;
+        // validationError = <p>{props.errorMessage}</p>;
+    }
     return (<div className={classes.Input}>
         <label className={classes.Label}>{props.label}</label>
         {inputElement}
+        {validationError}
     </div>)
 };
 
