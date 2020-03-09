c797d4983455fbe86e0bb82b22b18679f77861c3
blaec <odeskonst@yandex.ru>
2020-03-09 23:20:16 +0200

    343. Handling Purchases & Updating the UI

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,7 +3,7 @@ import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSumm
 import {Route, Redirect} from "react-router-dom";
 import ContactData from "./ContactData/ContactData";
 import {connect} from "react-redux";
-import * as actionTypes from "../../store/actions/actionTypes";
+import * as actions from "../../store/actions/index";
 
 class Checkout extends Component {
 
@@ -17,8 +17,10 @@ class Checkout extends Component {
     render() {
         let summary = <Redirect to='/'/>;
         if (this.props.ings) {
+            const purchasedRedirect = this.props.purchased ? <Redirect to="/" /> : null;
             summary = (
                 <div>
+                    {purchasedRedirect}
                     <CheckoutSummary ingredients={this.props.ings}
                                      checkoutCancelled={this.checkoutCancelledHandler}
                                      checkoutContinued={this.checkoutContinuedHandler}/>
@@ -33,7 +35,8 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.burgerBuilder.ingredients
+        ings: state.burgerBuilder.ingredients,
+        purchased: state.order.purchased
     }
 };
 
