e841b7dec75779747efe2b8f282b668b6c5c028d
blaec <odeskonst@yandex.ru>
2020-03-26 00:09:59 +0200

    422. Diving Deeper into Sagas

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,13 +1,13 @@
-import {delay, put} from 'redux-saga/effects';
+import {delay, put, call} from 'redux-saga/effects';
 import * as actions from "../actions/index";
 import axios from "axios";
 import {authFail, authSuccess, checkAuthTimeout} from "../actions/auth";
 import {logout} from "../actions/index";
 
 export function* logoutSaga(action) {
-    yield localStorage.removeItem('token');
-    yield localStorage.removeItem('expirationDate');
-    yield localStorage.removeItem('userId');
+    yield call([localStorage, 'removeItem'], 'token');
+    yield call([localStorage, 'removeItem'], 'expirationDate');
+    yield call([localStorage, 'removeItem'], 'userId');
     yield put(actions.logoutSucceed());
 }
 
