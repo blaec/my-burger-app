69faa48b588de0d69fccd418b1cad20c7e159149
blaec <odeskonst@yandex.ru>
2020-03-26 00:01:34 +0200

    420. Moving the Orders Side Effects into Sagas

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,6 +1,7 @@
 import {takeEvery} from 'redux-saga/effects';
 import {authCheckStateSaga, authUserSaga, checkAuthTimeoutSaga, logoutSaga} from './auth';
 import {initIngredientsSaga} from './burgetBuilder';
+import {purchaseBurgerSaga, fetchOrdersSaga} from './order';
 import * as actionTypes from '../actions/actionTypes';
 
 export function* watchAuth() {
@@ -12,4 +13,9 @@ export function* watchAuth() {
 
 export function* watchBurgerBuilder() {
     yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga);
+}
+
+export function* watchOrder() {
+    yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
+    yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga);
 }
